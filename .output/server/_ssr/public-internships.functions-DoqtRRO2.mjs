c1f9ced086d05eff6ad9108fd0e8b5fcfe950705
pchetanchandra <pchetanchandra@gmail.com>
2026-08-23 11:11:59 +0530

    fix: ignore node_modules, build artifacts, and env secrets

--- .output/server/_ssr/public-internships.functions-DoqtRRO2.mjs
+++
@@ -1,7 +0,0 @@
-import { c as createServerFn } from "./createServerFn-CIHAFgYl.mjs";
-import { t as createSsrRpc } from "./createSsrRpc-O6mcWyOU.mjs";
-//#region node_modules/.nitro/vite/services/ssr/assets/public-internships.functions-DoqtRRO2.js
-var listPublicInternships = createServerFn({ method: "GET" }).handler(createSsrRpc("bb9a29aa8f83b6236ec85a32133133234e5380571504577e889e1f28c67944c5"));
-var getPublicInternship = createServerFn({ method: "GET" }).inputValidator((input) => input).handler(createSsrRpc("5d4b40d7330b21f981f5c398c0905a6f67ed3f675dba00e95e490fab1590f781"));
-//#endregion
-export { listPublicInternships as n, getPublicInternship as t };
